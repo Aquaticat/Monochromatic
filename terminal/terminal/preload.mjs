ba5b5f3c9aa386378393c3c6ab5ab47063a1ccf9
Aquaticat <an@aquati.cat>
2024-03-17 21:40:11 -0400

    remove broken terminal

--- terminal/terminal/preload.mjs
+++
@@ -1,9 +0,0 @@
-// biome-ignore : otherwise swallows whole file
-import { contextBridge, ipcRenderer } from 'electron';
-
-window.addEventListener('DOMContentLoaded', () => {});
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  onExecute: (callback) => ipcRenderer.on('onExecute', (_event, value) => callback(value)),
-  execute: (value) => ipcRenderer.send('execute', value),
-});
