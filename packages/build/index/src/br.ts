2d714df053f80a4f5a7a93fcbfda71eab10dab55
Aquaticat <an@aquati.cat>
2024-05-09 21:59:18 -0400

    explicitly use bash shell

--- packages/build/index/src/br.ts
+++ packages/build/index/src/br.ts
@@ -3,8 +3,8 @@ import { path } from '@monochromatic.dev/module-fs-path';
 import { Glob } from 'glob';
 import { mapParallelAsync } from 'rambdax';
 import $c from './child.ts';
-import type { State } from './state.ts';
 import g from './g.ts';
+import type { State } from './state.ts';
 
 const l = getLogger(['build', 'br']);
 
@@ -14,7 +14,12 @@ export default async function brStatic(globCache = g()): Promise<State> {
     !['br', 'map', 'd.ts'].some((dotlessNoCompressExt) => staticFilePath.endsWith(`.${dotlessNoCompressExt}`))
   );
   l.debug`br ${staticFilePaths}`;
-  return ['brStatic','SUCCESS', await mapParallelAsync(async function brStatic(staticFilePath) {
-    return (await $c(`brotli -v -s -f -q 11 ${path.resolve(staticFilePath)}`)).stdout;
-  }, staticFilePaths)];
+  return [
+    'brStatic',
+    'SUCCESS',
+    await mapParallelAsync(async function brStatic(staticFilePath) {
+      // We're not enabling the squash command line flag because ubuntu-latest (22.04) on GitHub Actions still ships with 1.0.9
+      return (await $c(`brotli -v -f -q 11 ${path.resolve(staticFilePath)}`)).stdout;
+    }, staticFilePaths),
+  ];
 }
