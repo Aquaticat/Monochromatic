c05a1b549688dc0aa637b3326d33bd5275f10ab5
Aquaticat <an@aquati.cat>
2025-08-21 17:35:36 -0400

    feat(jsonc): add specs

--- packages/module/es/src/type/custom/string/jsonc/basic.ts
+++ packages/module/es/src/type/custom/string/jsonc/basic.ts
@@ -1,13 +1,102 @@
 export type jsonc = string & {__brand: 'jsonc'};
 
-//
-//```jsonc
+// Try not to use any in-package function.
+
+// const myJsonc
+// ```jsonc
 // // comment1
+// // more comment 1
 // {
-// // comment2
-// "a": /*comment3*/ 1,
-// "b": 2,
-// },
+//   // comment2
+//   "a": /*comment3*/ 1,
+//   "b": 2,
+//   "c": [
+//     1,
+//     2,
+//     // comment4
+//     /* comment4 mixed */
+//     3,
+//     // comment5 trailing comma ignored
+//     'd',
+//   ]
+// }
 // ```
 //
-// {//}
+// const myParsedJsonc = jsoncToParsedJsonc(myJsonc)
+// {
+//   type: 'record'
+//   comment: {type: 'inline', commentValue: ' comment1\n more comment 1'},
+//   value: [
+//      {
+//         recordKey: {
+//           comment: {type: 'inline', value: ' comment2'},
+//           type: 'string'
+//           value: 'a',
+//         },
+//         recordValue: {
+//           comment: {type: 'block', value: 'comment3'},
+//           type: 'number'
+//           value: 1,
+//         }
+//      },
+//      {
+//        recordKey: {
+//          type: 'string',
+//          value: 'b'
+//        },
+//        recordValue: {
+//          type: 'number'
+//          value: 2
+//        }
+//      },
+//      {
+//        recordKey: {
+//          type: 'string',
+//          value: 'c'
+//        },
+//        recordValue: {
+//          type: 'array',
+//          value: [
+//            {
+//              type: number,
+//              value: 1
+//            },
+//            {
+//              type: number,
+//              value: 2
+//            },
+//            {
+//              type: number,
+//              comment: {type: 'mixed', commentValue: ' comment4\n comment4 mixed '},
+//              value: 3
+//            },
+//            {
+//              type: string,
+//              value: 'd'
+//            },
+//          ]
+//        }
+//      }
+//   ]
+// }
+//
+// const reserializedJsonc = parsedJsoncToJsonc(myParsedJsonc)
+// ```jsonc
+// // comment1
+// // more comment1
+// {
+//   // comment2
+//   "a": /*comment3*/ 1,
+//   "b": 2,
+//   "c": [
+//     1,
+//     2,
+//     // comment4
+//     // comment4 mixed
+//     3,
+//     // comment5 trailing comma ignored
+//     'd'
+//   ]
+// }
+// ```
+
