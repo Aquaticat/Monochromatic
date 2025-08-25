373bc06bede039fa4e45c086ebdbb54e50ffa808
Aquaticat <an@aquati.cat>
2025-08-25 12:49:51 -0400

    refactor

--- packages/module/es/src/types/type string/from/index.ts
+++ packages/module/es/src/types/type string/from/index.ts
@@ -1,8 +1,3 @@
 // Re-export all string-producing functions
 export * from '../type typeof/from/type unknown/index.ts';
-export * from './async/index.ts';
-export * from './css/index.ts';
-export * from './path/index.ts';
-export * from './random/index.ts';
 export * from './type array/index.ts';
-export * from './type string/index.ts';
